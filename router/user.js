23ab6c76e11231e6ee2ad47e5c2cec82b882c27e
unknown <saxenaaman903@gmail.com>
2023-11-07 00:23:00 +0530

    login

--- router/user.js
+++ router/user.js
@@ -3,9 +3,11 @@ const express = require("express");
 const router = express.Router();
 
 // const{createNewUser } = require("../controller/user")
-const { registerUser , LoginUser  } = require("../controller/Auth");
+const { registerUser , LoginUser  } = require("../controller/user");
 
 router.post("/sigup" , registerUser)
 
 router.post("/login" , LoginUser)
+
+
 module.exports = router
